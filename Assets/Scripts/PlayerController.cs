d41cd1da5399925d80647b80ac221ca7cb5a6276
JungHo Park <haplane5@gmail.com>
2022-05-14 19:24:55 +0900

    Terrain

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,6 +5,10 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     public Animator animator;
+    public float speed;
+    public Rigidbody rigid;
+    public bool isInteract = false;
+
 
     // Start is called before the first frame update
     void Start()
@@ -15,23 +19,42 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Vector3 dir = Vector3.zero;
+        
         if (Input.GetKeyDown(KeyCode.V))
         {
             animator.SetBool("IsField", !animator.GetBool("IsField"));
+            speed *= (animator.GetBool("IsField") ? 2f : 0.5f);
         }
 
-        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        if (!isInteract)
         {
-            animator.SetBool("IsMove", true);
-        }
-        else
-        {
-            animator.SetBool("IsMove", false);
+            var dirX = Input.GetAxisRaw("Horizontal");
+            var dirZ = Input.GetAxisRaw("Vertical");
+
+            if (dirX != 0 || dirZ != 0)
+            {
+                animator.SetBool("IsMove", true);
+                rigid.velocity = (transform.forward * dirZ + transform.right * dirX).normalized * speed;
+            }
+            else
+            {
+                animator.SetBool("IsMove", false);
+            }
         }
 
         if (Input.GetMouseButtonDown(0))
         {
             animator.SetTrigger("Interact");
+            StartCoroutine(co_Interact());
         }
+    }
+
+    IEnumerator co_Interact()
+    {
+        isInteract = true;
+        yield return new WaitForEndOfFrame();
+        yield return new WaitForSeconds(animator.GetCurrentAnimatorStateInfo(0).length);
+        isInteract = false;
     }
 }
